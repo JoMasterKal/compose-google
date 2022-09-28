776f36c7e7a52a15c60e30f990cb24e6f2b2c078
jonathan kalonga <jokalmwamba@gmail.com>
2022-09-28 16:08:26 +0200

    add string resources and add text to view

--- taskmanager/src/main/java/com/covate/taskmanager/MainActivity.kt
+++ taskmanager/src/main/java/com/covate/taskmanager/MainActivity.kt
@@ -3,13 +3,20 @@ package com.covate.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.covate.taskmanager.ui.theme.ComposeGoogleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    TaskFinish()
                 }
             }
         }
@@ -30,14 +38,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskFinish() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null)
+        
+        Text(text = stringResource(R.string.task_completed_text),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+        )
+
+        Text(text = stringResource(R.string.nice_work_text),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+        )
+
+    }
 
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun DefaultPreview() {
     ComposeGoogleTheme {
-
+        TaskFinish()
     }
 }
