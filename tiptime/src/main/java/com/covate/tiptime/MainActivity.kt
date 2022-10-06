9e7162b05a11cd0e9186744825242db727c73064
jonathan kalonga <jokalmwamba@gmail.com>
2022-10-06 14:42:59 +0200

    add title text and editField

--- tiptime/src/main/java/com/covate/tiptime/MainActivity.kt
+++ tiptime/src/main/java/com/covate/tiptime/MainActivity.kt
@@ -3,13 +3,18 @@ package com.covate.tiptime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.covate.tiptime.ui.theme.ComposeGoogleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TipTImeScreen()
                 }
             }
         }
@@ -30,14 +35,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TipTImeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(Modifier.height(16.dp))
+        EditNumberField()
+
+
+    }
+}
+
+@Composable
+fun EditNumberField() {
+    TextField(
+        value = "",
+        onValueChange = {},
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeGoogleTheme {
-        Greeting("Android")
+        TipTImeScreen()
     }
 }
