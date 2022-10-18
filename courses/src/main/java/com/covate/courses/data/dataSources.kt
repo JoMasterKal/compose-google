354d4ed1bb96740f90e617d3d0fe746451b29950
jonathan kalonga <jokalmwamba@gmail.com>
2022-10-18 16:19:07 +0200

    create data source file

---
+++ courses/src/main/java/com/covate/courses/data/dataSources.kt
@@ -0,0 +1,23 @@
+package com.covate.courses.data
+
+import com.covate.courses.R
+import com.covate.courses.model.Branch
+
+object DataSource {
+    val topics = listOf(
+        Branch(R.drawable.architecture,R.string.architecture_text, 58),
+        Branch( R.drawable.crafts,R.string.crafts_text, 121),
+        Branch(R.drawable.business,R.string.business_text, 78),
+        Branch( R.drawable.culinary,R.string.culinary_text, 118),
+        Branch(R.drawable.design,R.string.design_text, 423),
+        Branch(R.drawable.fashion,R.string.fashion_text, 92),
+        Branch(R.drawable.film,R.string.film_text, 165),
+        Branch( R.drawable.gaming,R.string.gaming_text, 164),
+        Branch(R.drawable.drawing,R.string.drawing_text, 326),
+        Branch(R.drawable.lifestyle,R.string.lifestyle, 305),
+        Branch(R.drawable.music,R.string.music_text, 212),
+        Branch(R.drawable.painting,R.string.painting_text, 172),
+        Branch(R.drawable.photography,R.string.photography_text, 321),
+        Branch(R.drawable.tech,R.string.tech_text, 118)
+    )
+}
