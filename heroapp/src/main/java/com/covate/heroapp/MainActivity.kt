fc026c05d34dd000a0d01968cfdc7afbf4e4a77c
jonathan kalonga <jokalmwamba@gmail.com>
2022-10-22 12:27:45 +0200

    finish a lazyColum of superHero

--- heroapp/src/main/java/com/covate/heroapp/MainActivity.kt
+++ heroapp/src/main/java/com/covate/heroapp/MainActivity.kt
@@ -3,41 +3,78 @@ package com.covate.heroapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.covate.heroapp.ui.theme.ComposeGoogleTheme
+import androidx.compose.ui.unit.dp
+import com.covate.heroapp.model.HeroesRepository
+import com.covate.heroapp.ui.theme.HeroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeGoogleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            HeroTheme {
+                HeroApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HeroApp() {
+
+    Scaffold(
+        topBar = { HeroAppBar() }
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .background(MaterialTheme.colors.background)
+                .padding(it)
+        ) {
+            items(HeroesRepository.heroes) {
+                HeroCard(hero = it)
+            }
+        }
+    }
+
+
+}
+
+
+@Composable
+fun HeroAppBar(modifier: Modifier = Modifier) {
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp),
+    ) {
+        Text(
+            text = stringResource(id = R.string.hero_title),
+            style = MaterialTheme.typography.h1,
+
+            )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeGoogleTheme {
-        Greeting("Android")
+    HeroTheme {
+        HeroApp()
     }
 }
