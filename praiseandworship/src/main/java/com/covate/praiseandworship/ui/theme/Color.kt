6047984154861c1419f41cd3708dd70c7521a54e
jonathan kalonga <jokalmwamba@gmail.com>
2022-10-22 19:31:28 +0200

    add color

--- praiseandworship/src/main/java/com/covate/praiseandworship/ui/theme/Color.kt
+++ praiseandworship/src/main/java/com/covate/praiseandworship/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package com.covate.praiseandworship.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+
+
+val primaryColor = Color(0xFF4FC2F7)
+val primaryLightColor = Color(0xFF8BF5FF)
+val primaryDarkColor= Color(0xFF0092C4)
+val secondaryColor = Color(0xFF1675D1)
+val secondaryLightColor = Color(0xFF62A3FF)
+val secondaryDarkColor = Color(0xFF004A9F)
+val primaryTextColor = Color(0xFF000000)
+val secondaryTextColor = Color(0xFFFFFFFF)
