7da3a76d4cabbc6b6d5a833ccd46904e0095e906
jonathan kalonga <jokalmwamba@gmail.com>
2022-10-23 15:06:28 +0200

    add lazy column

--- praiseandworship/src/main/java/com/covate/praiseandworship/MainActivity.kt
+++ praiseandworship/src/main/java/com/covate/praiseandworship/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.covate.praiseandworship.model.WorshipRepository
 import com.covate.praiseandworship.ui.theme.PraiseTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,16 +22,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PraiseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
+}
+
+@Composable
+fun ListWorship(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier.padding(8.dp)
+    ) {
+        items(WorshipRepository.praises){
+            PraiseCard(praise = it)
+        }
+    }
+
 }
 
 @Composable
@@ -38,6 +49,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     PraiseTheme {
-        Greeting("Android")
+        ListWorship()
     }
 }
