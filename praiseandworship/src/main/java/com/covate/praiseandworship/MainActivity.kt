11358a3e775382596fe43c26863dfec5ec18f4bb
jonathan kalonga <jokalmwamba@gmail.com>
2022-10-23 16:28:08 +0200

    add AppTop bar

--- praiseandworship/src/main/java/com/covate/praiseandworship/MainActivity.kt
+++ praiseandworship/src/main/java/com/covate/praiseandworship/MainActivity.kt
@@ -3,14 +3,19 @@ package com.covate.praiseandworship
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PraiseTheme {
-
+                ListWorship()
             }
         }
     }
@@ -30,19 +35,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ListWorship(modifier: Modifier = Modifier) {
-    LazyColumn(
-        modifier.padding(8.dp)
-    ) {
-        items(WorshipRepository.praises){
-            PraiseCard(praise = it)
+
+    Scaffold(
+        topBar = { TopAppBar()}
+        ) {
+        LazyColumn(
+            modifier
+                .padding(it)
+                .background(MaterialTheme.colors.background)
+        ) {
+            items(WorshipRepository.praises){
+                PraiseCard(praise = it)
+            }
         }
     }
 
 }
 
+
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopAppBar() {
+    Row(Modifier.padding(start = 16.dp)
+        .height(56.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Adoration",
+            style = MaterialTheme.typography.h1
+        )
+    }
 }
 
 @Preview(showBackground = true)
